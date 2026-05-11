8bf82118d7b65dab17a204411ed6e85157870601
phongsathonepakse-boop <phongsathonepakse@gmail.com>
2026-05-12 02:21:08 +0700

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Security.Principal;
+using WebBanDienThoai.Models;
+
+namespace WebBanDienThoai
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // This tells EF to stop checking for model changes and just use the database as-is.
+            Database.SetInitializer<WebBanDienThoai.Models.AppDbContext>(null);
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PostAuthenticateRequest()
+        {
+            var authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null || string.IsNullOrWhiteSpace(authCookie.Value))
+            {
+                return;
+            }
+
+            var ticket = FormsAuthentication.Decrypt(authCookie.Value);
+            if (ticket == null || ticket.Expired)
+            {
+                return;
+            }
+
+            var roles = string.IsNullOrWhiteSpace(ticket.UserData) ? new string[0] : ticket.UserData.Split(',');
+            Context.User = new GenericPrincipal(new FormsIdentity(ticket), roles);
+        }
+    }
+}
